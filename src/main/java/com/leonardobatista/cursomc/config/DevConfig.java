68ecbbc30110a925f2d445825ba65859b5d0a62b
Leonardo Batista Carias <leonardo1carias@gmail.com>
2019-06-30 00:15:10 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/leonardobatista/cursomc/config/DevConfig.java
+++ src/main/java/com/leonardobatista/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.leonardobatista.cursomc.services.DBService;
+import com.leonardobatista.cursomc.services.EmailService;
+import com.leonardobatista.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,6 +30,11 @@ public class DevConfig {
 		}
 		dbService.InstantiateTesteDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
