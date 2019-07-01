e8318ac9cb894e4ee97fc5e4911d997686532170
Leonardo Batista Carias <leonardo1carias@gmail.com>
2019-06-30 23:26:58 -0300

    Expondo o header Authorization

--- src/main/java/com/leonardobatista/cursomc/resources/AuthResources.java
+++ src/main/java/com/leonardobatista/cursomc/resources/AuthResources.java
@@ -31,13 +31,13 @@ public class AuthResources {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
 		return ResponseEntity.noContent().build();
 	}	
 
 	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
 	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
 		service.sendNewPassword(objDto.getEmail());
-		
 		return ResponseEntity.noContent().build();
 	}
 }
